730aa680dd7d293082b4efb3645f8f373e3a95b2
Jose <ja.moratalla.2020@alumnos.urjc.es>
2024-05-29 14:31:46 +0200

    Localhost funcional

--- Fase3/src/main/java/com/payback/demo/PayBackApplication.java
+++ Fase3/src/main/java/com/payback/demo/PayBackApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PayBackApplication{
 	
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApplication.class, args);
+		SpringApplication.run(PayBackApplication.class, args);
 	}
 	
 }
